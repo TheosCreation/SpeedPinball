021db9a2c92bd4ca39703a1ed0673d0410fb3203
Theo Morris <pandazflipperpanda706@gmail.com>
2024-02-08 21:03:56 +1300

    collisions now in

--- Assets/Scripts/scr_Move.cs
+++ Assets/Scripts/scr_Move.cs
@@ -10,21 +10,32 @@ public class scr_Move : MonoBehaviour
 {
     // Start is called before the first frame update
     private Rigidbody2D m_rb;
+    //private CircleCollider2D m_circleCollider2D;
+    private Vector2 m_lastVelocity;
     public float m_speed = 0.1f;
     public float m_speedCap = 5.0f;
     public float m_turnDelay = 0.5f; //sets speed to this number when turning so slide aint that bad
     public float m_turnSpeed =  0.4f;
+    public Vector3 m_direction;
+
+    
  
     void Start()
     {
        m_rb = GetComponent<Rigidbody2D>();
+       //m_circleCollider2D = GetComponent<CircleCollider2D>();
     }
 
     // Update is called once per frame
     
     void Update()
     {
+        m_lastVelocity = m_rb.velocity;
+        Movement();
+    }
 
+    private void Movement()
+    {
         if (Input.GetKey(KeyCode.W) && m_rb.velocity.y < m_speedCap)
         {
             float t_speed = 0; //temp speed 
@@ -85,6 +96,11 @@ public class scr_Move : MonoBehaviour
             }
             m_rb.velocity = new Vector2(m_rb.velocity.x - t_speed, m_rb.velocity.y);
         }
+    }
 
+    private void OnCollisionEnter2D(Collision2D Collision)
+    {
+        m_direction = Vector2.Reflect(m_lastVelocity.normalized, Collision.contacts[0].normal);
+        m_rb.velocity = m_direction * Mathf.Max(m_lastVelocity.magnitude, 0f);
     }
 }
