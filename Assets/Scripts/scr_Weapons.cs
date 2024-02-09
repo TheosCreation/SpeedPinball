d22d1652f04cf5331dfe5200312f4cf662515a62
k4x1MDS <141291975+k4x1MDS@users.noreply.github.com>
2024-02-09 19:50:09 +1300

    shatgun

--- Assets/Scripts/scr_Weapons.cs
+++ Assets/Scripts/scr_Weapons.cs
@@ -12,6 +12,9 @@ public class scr_Weapons : MonoBehaviour
     public bool assaultRifle = false;
     public bool bomba = false;
     public int m_CurrentWeapon = 0;
+    public float m_shotgunSpread = 10.0f;
+    public int m_shotgunCount = 5;
+    public int m_shotgunRecoil = 5;
     [SerializeField]private Transform m_gunPoint;
     [SerializeField] private GameObject m_bulletTrail;
     [SerializeField] private GameObject m_cameraRef;
@@ -38,6 +41,7 @@ public class scr_Weapons : MonoBehaviour
         switch (m_CurrentWeapon)
         {
             case 0:
+                { 
                 m_cameraRef.GetComponent<scr_Camera>().ScreenShake(0.1f, 0.25f);
                 var hit = Physics2D.Raycast(
                     m_gunPoint.position,
@@ -66,9 +70,51 @@ public class scr_Weapons : MonoBehaviour
 
                 //pistol
                 break;
+                }
             case 1:
-                //shotgun
-                break;
+                {
+                    //shotgun
+                 
+                    m_cameraRef.GetComponent<scr_Camera>().ScreenShake(0.25f, 0.5f);
+                    for (int i = -m_shotgunCount; i < m_shotgunCount; i++)
+                    {
+                        // Calculate a random offset for each bullet
+                        Vector3 randomOffset = new Vector3(Random.Range(-m_shotgunSpread, m_shotgunSpread), Random.Range(-m_shotgunSpread, m_shotgunSpread), 0f);
+
+                        // Adjust the direction vector with the random offset
+                        Vector3 adjustedDirection = transform.up + randomOffset;
+
+                        var hit = Physics2D.Raycast(
+                            m_gunPoint.position,
+                            adjustedDirection,
+                            m_weaponRange,
+                            LayerMask.NameToLayer("Player")
+                        );
+
+                        var trail = Instantiate(
+                            m_bulletTrail,
+                            m_gunPoint.position,
+                            transform.rotation
+                        );
+
+                        var trailScript = trail.GetComponent<scr_BulletTrail>();
+                        if (hit.collider != null)
+                        {
+                            trailScript.SetTargetPos(hit.point);
+                        }
+                        else
+                        {
+                            var endPos = m_gunPoint.position + adjustedDirection * m_weaponRange;
+                            trailScript.SetTargetPos(endPos);
+                        }
+                    }
+
+
+
+                    rb.velocity = new Vector2(rb.velocity.x - transform.up.x * m_shotgunRecoil, rb.velocity.y - transform.up.y * m_shotgunRecoil);        
+                
+                    break;
+                }
             case 2:
                 //ar
                 break;
